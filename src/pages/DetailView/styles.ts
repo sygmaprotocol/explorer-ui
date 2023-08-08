9503007011429a8b270303afc8d0cfde0e246b2e
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2023-08-07 23:09:35 -0400

    padding to the badge

--- src/pages/DetailView/styles.ts
+++ src/pages/DetailView/styles.ts
@@ -84,7 +84,7 @@ export const useStyles = makeStyles()((theme) => {
     statusPill: {
       borderRadius: 32,
       background: "#E2D9CC",
-      width: 90,
+      padding: '4px 8px 4px 4px',
       display: 'flex',
       flexDirection: 'row',
       alignItems: 'center',
