09d7686559de1ed373f04df38c45ce92775e8fdf
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2024-09-30 12:19:47 -0300

    chore: small update on alt for images

--- src/utils/renderUtils.tsx
+++ src/utils/renderUtils.tsx
@@ -37,16 +37,16 @@ export const renderNetworkIcon = (caipId: string, classes: Record<"networkIcon" 
       return <img src={`/assets/icons/gnosis.svg`} alt="gnosis" className={classes.networkIcon} />
     case "eip155:421614":
     case "eip155:42161":
-      return <img src={`/assets/icons/arbitrum.svg`} alt="gnosis" className={classes.networkIcon} />
+      return <img src={`/assets/icons/arbitrum.svg`} alt="arbitrum" className={classes.networkIcon} />
     case "eip155:84532":
-      return <img src={`/assets/icons/base.svg`} alt="gnosis" className={classes.networkIcon} />
+      return <img src={`/assets/icons/base.svg`} alt="base" className={classes.networkIcon} />
     case "polkadot:3799":
-      return <img src={`/assets/icons/tangle-logo.svg`} alt="gnosis" className={classes.networkIcon} />
+      return <img src={`/assets/icons/tangle-logo.svg`} alt="tangle" className={classes.networkIcon} />
     case "eip155:1993":
     case "eip155:8333":
-      return <img src={`/assets/icons/b3-sepolia.svg`} alt="gnosis" className={classes.networkIcon} />
+      return <img src={`/assets/icons/b3-sepolia.svg`} alt="b3-sepolia" className={classes.networkIcon} />
     case "eip155:10":
-      return <img src={`/assets/icons/optimism.svg`} alt="gnosis" className={classes.networkIcon} />
+      return <img src={`/assets/icons/optimism.svg`} alt="optimism" className={classes.networkIcon} />
     default:
       return <img src={`/assets/icons/evm.svg`} alt="ethereum" className={classes.networkIcon} />
   }
